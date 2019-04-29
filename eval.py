fdc6f39d64b683edb175632a3475330699322cfd
Kai Xuan <woxuankai@gmail.com>
2019-04-29 16:33:37 +0800

    finished interpolation of segmentations

--- eval.py
+++ eval.py
@@ -1,11 +1,9 @@
-import os, glob
 import torch
-from   torchvision.utils import make_grid, save_image
-from   torchvision import transforms
+from torchvision.utils import make_grid, save_image
 import numpy as np
 import argparse
-from   model import IntroVAE
-import PIL
+from model import IntroVAE
+from main import DB, colormap
 
 def main(args):
     print(args)
@@ -17,32 +15,25 @@ def main(args):
     vae.load_state_dict(torch.load(args.load))
     print('load ckpt from:', args.load)
 
-    transform = transforms.Compose([
-        transforms.Resize([args.imgsz, args.imgsz]),
-        transforms.ToTensor()])
-
-    imgs = [transform(PIL.Image.open(img)) for img in args.input]
+    args.root = '/dev/null'
+    db = DB(args)
+    db.images = args.input
+    imgs = [img for img in db]
     x = torch.stack(imgs, dim=0).to(device)
-    if x.shape[1] == 1:
-        x = x.expand(-1,3,-1,-1)
     mu, logvar = vae.encoder(x)
 
     z = torch.nn.functional.interpolate(mu.permute(1,0).unsqueeze(0), \
-            size=args.n_interp, mode='linear').squeeze(0).permute(1,0)
-    #z = z.reshape(mu.shape[0], -1, *mu.shape[2:])
-    #z = []
-    #for i in range(args.n_interp):
-    #    z.append((mu[0]*(args.n_interp-i) + mu[1]*i)/float(args.n_interp))
-    #z = torch.stack(z, dim=0)
+            size=args.n_interp, mode='linear', align_corners=False \
+            ).squeeze(0).permute(1,0)
     xr = vae.decoder(z)
+    if args.num_classes >= 0:
+        x, xr = [colormap[img.argmax(1)].permute(0,3,1,2) \
+                for img in (x, xr)]
     max_len = max(x.shape[0], xr.shape[0])
-    #x = x[:] + [torch.zeros_like(x[0])]*(max_len - x.shape[0])
-    #xr = xr[:] + [torch.zeros_like(xr[0])]*(max_len - xr.shape[0])
     output = torch.cat( \
             [x[:]] + [torch.zeros_like(x[0:1])]*(max_len - x.shape[0]) + \
             [xr[:]] + [torch.zeros_like(xr[0:1])]*(max_len - xr.shape[0]), \
             dim=0)
-    #output = torch.cat([x[0:1], xr, x[1:2]], dim=0)
     save_image(output, args.output, nrow=max_len, range=(0,1))
     print('write output to :', args.output)
 
@@ -60,6 +51,8 @@ if __name__ == '__main__':
             help='checkpoint to load')
     argparser.add_argument('--output', type=str, required=True, \
             help='output path')
+    argparser.add_argument('--num_classes', type=int, default=-1, \
+            help='set to positive value to model shapes (e.g. segmentation)')
 
     args = argparser.parse_args()
     main(args)
